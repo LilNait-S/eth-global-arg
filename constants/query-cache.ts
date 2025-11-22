2634f7e20d17b477a7e78aa55f0a9aa511805f72
Nait-s <lilnait.santos@gmail.com>
2025-11-22 17:40:31 -0500

    feat: add getPoapsByEventId to BASE_CACHE_KEYS for Poap queries

--- constants/query-cache.ts
+++ constants/query-cache.ts
@@ -2,6 +2,7 @@ export const BASE_CACHE_KEYS = {
   // Poap
   getPoapEventById: "getPoapEventById",
   getScanAddress: "getScanAddress",
+  getPoapsByEventId: "getPoapsByEventId",
 
   // Talent Protocol
   getTalentProtocolProfile: "getTalentProtocolProfile",
